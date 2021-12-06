80954c1b88200087accce0770aaf28c2f2dd7960
egorniak <ewagorniak@op.pl>
2021-12-06 13:03:00 +0100

    kod działa

--- js/main.js
+++ js/main.js
@@ -1,12 +1,7 @@
- document.querySelector('button').onclick = function() {
+console.log("JS podpięty");
+
+let button = document.querySelector('button');
+
+button.onclick = function() {
      alert('Klik działa!');
- }
- 
- 
-
-
-
-
-
-
-    
+} 
